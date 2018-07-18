0af9bad71ae20b5eb20fbe75909961d386b650d9
shirlaine <shirlaine.phang@gmail.com>
2018-07-18 16:51:18 +0800

    Add requester service to retrieve api response

---
+++ app/services/requester_service.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+
+class RequesterService
+  ## call this service to get the response
+  def initialize(url)
+    @url = url
+  end
+
+  def call
+    uri = URI(@url)
+    response = Net::HTTP.get(uri)
+    puts response
+  end
+end
