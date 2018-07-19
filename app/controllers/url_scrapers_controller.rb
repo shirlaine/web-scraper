f0a4905e9113b74540f893e88108bbab00ea8f16
shirlaine <shirlaine.phang@gmail.com>
2018-07-19 23:10:26 +0800

    Add cache backend service

--- app/controllers/url_scrapers_controller.rb
+++ app/controllers/url_scrapers_controller.rb
@@ -8,27 +8,36 @@ class UrlScrapersController < ApplicationController
   end
   
   def create
+
     @url = url_scraper_params[:name]
-
+    ## call service to dl txt file
     @content = UrlscrapeActivateService.new(@url).scrape
-    # @urlscraper = Webscraper.new(url_scraper_params)
-    ## TO DO
-    ### check 
-    ## doing RequesterService.new(url).call will give the response
-    ## you want to get the params(url input from view) into the RequesterService and get the service to give you back response
+    ## rails db save/update
+     @urlscraper = Webscraper.new(name: @url, content: @content)
+    # update
+    @existing_url = Webscraper.find_by(name: @url)
 
-    @urlscraper = Webscraper.new(name: @url, content: @content)
-    if @urlscraper.save
-      flash[:notice] = "Your url has been sucessfully saved"
-      ## scrape file frm URL
-      ## save content into text file
-      ## Download file 
+    if @existing_url.present?
+      @time_lapse = Time.now - @existing_url.updated_at
+      ## set at 30 seconds in order to save to database again
+      if @time_lapse > 12
+        @existing_url.update(name: @url, content: @content)
+        flash[:notice] = "Your url has been scraped again, content has been refreshed."
+      # render :new
+        redirect_to url_scrapers_path
+      else
+        flash[:notice] = "Your url has been just scraped ago, please wait #{30- @time_lapse.floor} seconds to try again."
+        render :new
+      end
+    # save
+    elsif @urlscraper.save 
+      flash[:notice] = "Your url has been sucessfully scraped for the first time."
       redirect_to url_scrapers_path
     else
       flash[:notice] = "Can't scrape if no URL!"
       render :new
     end
-      
+    ## check time now if time does not exceed, save to database 
   end
 
   private
--- app/services/requester_service.rb
+++ app/services/requester_service.rb
@@ -11,4 +11,17 @@ class RequesterService
     @response = Net::HTTP.get(uri)
     @response.encode('UTF-8', invalid: :replace, undef: :replace)
   end
+
+  ## question
+  #     ## how to catch error here if flash/ redirect doesn't work?
+  # def get
+  #   uri = URI(@url)
+  #   begin
+  #     @response = Net::HTTP.get(uri)
+  #   rescue Timeout::Error, Errno::EINVAL, Errno::ECONNREFUSED, Errno::ECONNRESET, EOFError,
+  #     Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+  #     raise StandardError
+  #   end
+  #   @response.encode('UTF-8', invalid: :replace, undef: :replace)
+  # end
 end
